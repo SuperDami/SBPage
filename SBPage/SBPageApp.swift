e0ae8c38e1c1bf876041b83381e501d1ce8421fd
zhejun.chen <devsuperdami@gmail.com>
2025-02-23 11:46:02 +0900

    Initial Commit

---
+++ SBPage/SBPageApp.swift
@@ -0,0 +1,12 @@
+// Created by zhejun.chen on 2025/02/23
+
+import SwiftUI
+
+@main
+struct SBPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SBPageTests/SBPageTests.swift
@@ -0,0 +1,12 @@
+// Created by zhejun.chen on 2025/02/23
+
+import Testing
+@testable import SBPage
+
+struct SBPageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
